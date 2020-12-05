de2735a11501880530f4c97716924cc63d43db62
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-05 21:51:15 +0100

    dodawanie zmiennych

---
+++ App.java
@@ -0,0 +1,25 @@
+class App {
+	public static void main (String args[]){
+		//zadanie 1
+		System.out.println("------------Zadanie 1--------------");
+		int birthYear = 2001;
+		char gender = 'K';
+		
+		System.out.println("Birth year: "+birthYear);
+		System.out.println("Gender: "+gender);
+		// zadanie 2
+		System.out.println("------------Zadanie 2--------------");
+		boolean isAdult = true;
+		double average = 3.5;
+		System.out.println("Is adult? "+isAdult);
+		System.out.println("Average :"+average);
+		// zadanie 3
+		System.out.println("------------Zadanie 3--------------");
+		short houseNumber = 545;
+		byte apartmentNumber = 47;
+		int accountNumber = 2000000;
+		System.out.println("House number : "+houseNumber);
+		System.out.println("Apartment number : "+apartmentNumber);
+		System.out.println("Account number : "+accountNumber);
+	}
+}
